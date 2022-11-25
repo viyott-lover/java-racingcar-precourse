16f87c88705539ea78d7c9f1e0dc7dfef4dc5cf5
viyott-lover <jackk0616@gmail.com>
2022-11-25 20:51:11 +0900

    feat(View): 라운드를 입력받는 메소드 구현

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -4,10 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class View {
     private static final String INPUT_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ROUNDS = "시도할 회수는 몇회인가요?";
 
     public String inputCars() {
         System.out.println(INPUT_CARS);
-        String input = Console.readLine();
-        return input;
+        String cars = Console.readLine();
+        return cars;
+    }
+
+    public String inputRounds() {
+        System.out.println(INPUT_ROUNDS);
+        String rounds = Console.readLine();
+        return rounds;
     }
 }
