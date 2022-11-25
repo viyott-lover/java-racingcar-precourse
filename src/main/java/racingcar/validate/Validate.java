af407e2c156ba1a4d124468d84159c1314083f92
viyott-lover <jackk0616@gmail.com>
2022-11-25 21:17:36 +0900

    refactor(Validate): 유효성 검증 클래스 수정

--- src/main/java/racingcar/validate/Validate.java
+++ src/main/java/racingcar/validate/Validate.java
@@ -2,7 +2,9 @@ package racingcar.validate;
 
 public class Validate {
 
-    private static final String BLANK_EXCEPTION = "[ERROR] 입력된 내용이 없습니다. 다시 입력하세요.";
+    private static final String BLANK_EXCEPTION = "[ERROR] 입력된 내용이 없습니다.";
+    private static final String NUMBER_EXCEPTION = "[ERROR] 입력이 숫자가 아닙니다.";
+    private static final String LETTER_EXCEPTION = "[ERROR] 입력이 문자가 아닙니다.";
 
     public static void isBlank(String input) {
         if(input.length() == 0) {
@@ -11,14 +13,14 @@ public class Validate {
     }
 
     public static void isNumber(String input) {
-        if(input.matches("[0-9]+")) {
-            throw new IllegalArgumentException(BLANK_EXCEPTION);
+        if(input.matches("[^0-9]+")) {
+            throw new IllegalArgumentException(NUMBER_EXCEPTION);
         }
     }
 
     public static void isLetter(String input) {
-        if(input.matches("[a-zA-Z,]+")) {
-            throw new IllegalArgumentException(BLANK_EXCEPTION);
+        if(input.matches("[^a-zA-Z,]+")) {
+            throw new IllegalArgumentException(LETTER_EXCEPTION);
         }
     }
 }
