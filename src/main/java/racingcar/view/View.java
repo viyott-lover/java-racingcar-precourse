4ba3bc1b83e9e6e90253a0337807de663c602c8f
viyott-lover <jackk0616@gmail.com>
2022-11-25 21:16:56 +0900

    refactor(View): 메소드 명확한 의미전달을 위해서 차를 참여자로 변경

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -3,18 +3,18 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class View {
-    private static final String INPUT_CARS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_PARTICIPANTS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ROUNDS = "시도할 회수는 몇회인가요?";
 
-    public String inputCars() {
-        System.out.println(INPUT_CARS);
-        String cars = Console.readLine();
-        return cars;
+    public String inputParticipants() {
+        System.out.println(INPUT_PARTICIPANTS);
+        String participants = Console.readLine();
+        return participants.trim();
     }
 
     public String inputRounds() {
         System.out.println(INPUT_ROUNDS);
         String rounds = Console.readLine();
-        return rounds;
+        return rounds.trim();
     }
 }
