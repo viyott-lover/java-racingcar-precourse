301ac0f8745daec80b86b9777511cf47325c3dfb
viyott-lover <jackk0616@gmail.com>
2022-11-25 23:00:07 +0900

    feat(View): 실행 결과를 출력해주는 메소드 작성

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -1,10 +1,14 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Car;
+
+import java.util.List;
 
 public class View {
     private static final String INPUT_PARTICIPANTS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ROUNDS = "시도할 회수는 몇회인가요?";
+    private static final String RESULT = "실행 결과";
 
     public String inputParticipants() {
         System.out.println(INPUT_PARTICIPANTS);
@@ -16,5 +20,25 @@ public class View {
         System.out.println(INPUT_ROUNDS);
         String rounds = Console.readLine();
         return rounds.trim();
+    }
+
+    public void printResult(List<Car> participants) {
+        System.out.println(RESULT);
+        for (Car participant : participants) {
+            System.out.println(participant.getName() + " : " + calculateMileage(participant));
+        }
+        System.out.println();
+    }
+
+    private String calculateMileage(Car participant) {
+        StringBuilder mileage = new StringBuilder();
+        for(int i = 0; i < participant.getPosition(); i++) {
+            mileage.append("-");
+        }
+        return mileage.toString();
+    }
+
+    public void printRound() {
+
     }
 }
