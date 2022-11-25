871305a73592b1552ac6be068b5f5a4f6963fce1
viyott-lover <jackk0616@gmail.com>
2022-11-25 23:09:24 +0900

    feat(View): 우승자를 출력해주는 메소드 작성

--- src/main/java/racingcar/view/View.java
+++ src/main/java/racingcar/view/View.java
@@ -4,11 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 import racingcar.model.Car;
 
 import java.util.List;
+import java.util.StringJoiner;
 
 public class View {
     private static final String INPUT_PARTICIPANTS = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ROUNDS = "시도할 회수는 몇회인가요?";
     private static final String RESULT = "실행 결과";
+    private static final String WINNERS = "최종 우승자 : ";
 
     public String inputParticipants() {
         System.out.println(INPUT_PARTICIPANTS);
@@ -32,13 +34,17 @@ public class View {
 
     private String calculateMileage(Car participant) {
         StringBuilder mileage = new StringBuilder();
-        for(int i = 0; i < participant.getPosition(); i++) {
+        for (int i = 0; i < participant.getPosition(); i++) {
             mileage.append("-");
         }
         return mileage.toString();
     }
 
-    public void printRound() {
-
+    public void printWinners(List<Car> winners) {
+        StringJoiner joiner = new StringJoiner(", ");
+        for (Car winner : winners) {
+            joiner.add(winner.getName());
+        }
+        System.out.println(WINNERS + joiner.toString());
     }
 }
