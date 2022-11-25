92cc44bd628f4405fa84bdf696909a9a751d0d4d
viyott-lover <jackk0616@gmail.com>
2022-11-25 20:17:42 +0900

    feat(Car): 전진 메소드 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -9,4 +9,15 @@ public class Car {
     }
 
     // 추가 기능 구현
+    public void move() {
+        this.position++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
