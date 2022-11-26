ab0fed3fba0b7323268cd78a73a9718a3a3c2f5d
viyott-lover <jackk0616@gmail.com>
2022-11-26 17:10:30 +0900

    feat(Round): 라운드에 대한 검증 메소드를 작성

--- src/main/java/racingcar/model/Round.java
+++ src/main/java/racingcar/model/Round.java
@@ -9,12 +9,15 @@ public class Round {
 
     Round(String round) {
         validateRound(round);
-        this.round = round;
+        this.round = Integer.parseInt(round);
     }
 
     private void validateRound(String round) {
         isBlank(round);
         isNumber(round);
+    }
 
+    public int getRound() {
+        return round;
     }
 }
