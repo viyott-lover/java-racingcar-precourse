c7091f5e4945da64fff63fdd18e5ab46b913a4bf
viyott-lover <jackk0616@gmail.com>
2022-11-26 17:10:59 +0900

    feat(Participants): 게임 참여 차량에 대한 검증 메소드를 작성

--- src/main/java/racingcar/model/Participant.java
+++ src/main/java/racingcar/model/Participant.java
@@ -1,7 +1,40 @@
 package racingcar.model;
 
+import racingcar.validate.Validate;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static racingcar.validate.Validate.*;
 
 public class Participant {
     private List<Car> participants;
+
+    Participant(String participants) {
+        isDuplicated(participants);
+        this.participants = splitParticipants(participants);
+    }
+
+    private List<Car> splitParticipants(String participants) {
+        List<Car> temp = new ArrayList<>();
+        validateParticipants(participants);
+        for (String name : participants.split(",")) {
+            temp.add(new Car(name.trim()));
+        }
+        return temp;
+    }
+
+    private void validateParticipants(String participants) {
+        isDuplicated(participants);
+        for (String participant : participants.split(",")) {
+            isBlank(participant);
+            isLetter(participant);
+            // 5글자인지
+            isFiveCharsOrLess(participant);
+        }
+    }
+
+    public List<Car> getParticipants() {
+        return participants;
+    }
 }
